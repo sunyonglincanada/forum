79186674191cbf0fd6470458a5f8adbfbbb109d2
Yonglin Sun <sunyonglincanada@gmail.com>
2017-10-06 10:41:22 -0400

    feat: login users can publish threads.

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/{thread}', 'ThreadsController@show');
+Route::post('/threads', 'ThreadsController@store');
 Route::post('/threads/{thread}/replies', 'RepliesController@store');
