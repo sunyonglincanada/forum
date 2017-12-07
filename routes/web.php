4a894eab26f46559a77ac6723574d10c2a63cfe7
Yonglin Sun <sunyonglincanada@gmail.com>
2017-12-07 15:30:13 -0500

    created user profile page

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,6 @@ Route::post('/threads', 'ThreadsController@store');
 Route::get('threads/{channel}', 'ThreadsController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
+
+// User Profile
+Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
